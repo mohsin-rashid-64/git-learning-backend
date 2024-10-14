cd6773cf62df70fae9f2a958519586bcb3e59c35
MuhammadAtif3-rgb <muhammad_atif@digimarkdevelopers.com>
2024-10-14 13:06:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return jsonify({"message": "Hello, World! YO MAN SOME OTTHER TIME"})
+    return jsonify({"message": "Hello, World! YO MAN SOME OTTHER TIME POWER"})
 
 if __name__ == "__main__":
     app.run(debug=True)
